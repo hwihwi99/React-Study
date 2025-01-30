27ec0448380606ec6ede47d1ceaf5c3b18af00b3
hwihwi99 <chamgrace@naver.com>
2025-01-30 17:07:38 +0900

    [한입크키로 잘라먹는 리액트 12강] 감정다이어리 만들기 - 폰트, 이미지, css 적용하기

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import NotFound from "./pages/NotFound";
+
+import { getEmotionImage } from "./util/get-emotion-image";
 /**
  * 1. "/" : 모든 일기를 조회하는 Home 페이지
  * 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -16,8 +18,29 @@ function App() {
   const onClickButton = () => {
     nav("/new");
   };
+
+  // assert 폴더에 넣으면 빌드 됨과 동시에 메모리 캐시에 저장되어서 다시 불려질 땐 그냥 캐시 가져오므로 0ms안에 불러진다.
+  // 하지만 public의 경우 매번 정적인 공간에서 불러와야해서 시간은 조금 소요되지만
+  // 너무 많은 이미지양을 저장해야할때는 assert에 넣는것은 오히려 메모리를 과다하게 잡아먹는 것으로
+  // 적은 이미지 : assert / 많은 이미지 : public 폴더에 넣어 진행하는 것이 좋다
+
   return (
     <>
+      <div>
+        <img src={getEmotionImage(1)} alt="이모지1" />
+        <img src={getEmotionImage(2)} alt="이모지2" />
+        <img src={getEmotionImage(3)} alt="이모지3" />
+        <img src={getEmotionImage(4)} alt="이모지4" />
+        <img src={getEmotionImage(5)} alt="이모지5" />
+      </div>
+      {/* 
+      <div>
+        <img src={"/emotion1.png"} alt="" />
+        <img src={"/emotion2.png"} alt="" />
+        <img src={"/emotion3.png"} alt="" />
+        <img src={"/emotion4.png"} alt="" />
+        <img src={"/emotion5.png"} alt="" />
+      </div> */}
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
